4b7586d98f2d28f90a96f73be9ff0dd7b1c22494
reshman <s.reshman@gmail.com>
2023-04-18 09:15:58 +0530

    upload folder creation

--- src/routes/pdf-converter.route.js
+++ src/routes/pdf-converter.route.js
@@ -6,9 +6,16 @@ const path = require('path');
 const authenticateToken = require('../middleware/jwt.verify');
 const multer = require('multer');
 const Joi = require('joi');
+const fs = require('fs');
+
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, path.join(__dirname, '../../public/uploads'), function(err, success) {
+    const uploadPath = path.join(__dirname, '../../public/uploads');
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
+    }
+
+    cb(null, uploadPath, function(err, success) {
       if(err) throw err;
       console.log('file', file)
     });
